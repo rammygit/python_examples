fd8c78a2a1eb4f2fe590717196225db57182d0f0
ramk <rammyramkumar@gmail.com>
2016-09-22 07:13:39 -0400

    added file

---
+++ assn1/mynewfile.py
@@ -0,0 +1,25 @@
+print "types in python!!"
+
+#types.
+print type('hello')
+print type(4.2)
+print type(4)
+
+# variables.
+principal = 62.80
+rate = 4
+interest = principal * rate
+print interest
+
+order = 1 + 2 **3/4 * 5
+print order
+
+print int('32')
+
+import math
+
+print math
+
+print math.factorial(4)
+print math.sqrt(4)
+
